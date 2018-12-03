4bb114c31925b2d1b0aa53866b51e901584f2fd8
Natalia Braz <naabraz@gmail.com>
2018-12-03 09:59:19 -0200

    Adding webpack config

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+let path = require('path')
+let nodeExternals = require('webpack-node-externals')
+
+const moduleObj = {
+  loaders: [
+    {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loaders: ["babel-loader"],
+    }
+  ],
+}
+
+const client = {
+  entry: {
+    'client': './src/client/index.js'
+  },
+  target: 'web',
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist/public')
+  },
+  module: moduleObj
+}
+
+const server = {
+  entry: {
+    'server': './src/server/index.js'
+  },
+  target: 'node',
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: moduleObj,
+  externals: [nodeExternals()]
+}
+
+module.exports = [client, server]
